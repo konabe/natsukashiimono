de668a5f577f89d99e248bef6f96bf579a5598e2
Ryota Konabe <rt.konabe@gmail.com>
2023-02-14 23:07:41 +0900

    Revert "CORS対策"
    This reverts commit c5cb6b110a69ea7b7c018471c76a93801e8b00a1.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
@@ -6,15 +6,10 @@ import { ContentController } from './content/content.controller';
 import { ContentModule } from './content/content.module';
 import { ScoreController } from './score/score.controller';
 import { ScoreModule } from './score/score.module';
-import { CorsMiddleware } from './cors.middleware';
 
 @Module({
   imports: [DatabaseModule, ContentModule, ScoreModule],
   controllers: [AppController, ContentController, ScoreController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(CorsMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
