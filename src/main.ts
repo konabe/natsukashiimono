d24ea5cddcdc66c60c44c496727156a28e87b3f7
Ryota Konabe <rt.konabe@gmail.com>
2023-03-07 21:27:26 +0900

    expressのインストール

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,3 +9,13 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
+
+// express移行後
+
+const app: express.Express = express();
+app.use(cors());
+const port = 9000;
+app.get('/', (_: express.Request, res: express.Response) => {
+  res.json({ message: 'ok' });
+});
+app.listen(port, () => console.log(`ok, port = ${port}`));
