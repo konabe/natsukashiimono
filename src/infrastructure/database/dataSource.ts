12bc1a0a56b962309db0397041eb2c98facdec31
Ryota Konabe <rt.konabe@gmail.com>
2023-03-08 22:31:22 +0900

    too many connections の修正

--- src/infrastructure/database/dataSource.ts
+++ src/infrastructure/database/dataSource.ts
@@ -2,18 +2,22 @@ import { DataSource } from 'typeorm';
 import { Score } from './score.entity';
 import { Content } from './content.entity';
 
+let dataSource: DataSource;
 export async function getDataSource() {
-  const dataSource = new DataSource({
-    type: 'mysql',
-    host: '127.0.0.1',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'natsukashiimono',
-    // FIXME: Windows10だとパスで指定する方法が通らないというバグがある。型の列挙で暫定対応
-    // entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-    entities: [Content, Score],
-    synchronize: true, // 本番ではfalseにするべき
-  });
-  return await dataSource.initialize();
+  if (dataSource == null) {
+    const _dataSource = new DataSource({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'natsukashiimono',
+      // FIXME: Windows10だとパスで指定する方法が通らないというバグがある。型の列挙で暫定対応
+      // entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      entities: [Content, Score],
+      synchronize: true, // 本番ではfalseにするべき
+    });
+    dataSource = await _dataSource.initialize();
+  }
+  return dataSource;
 }
