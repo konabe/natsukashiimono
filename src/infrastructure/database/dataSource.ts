910e53928992630dcbd5bf078d9527f2a8e03464
Ryota Konabe <rt.konabe@gmail.com>
2023-04-10 23:12:40 +0900

    move secret file to dotenv

--- src/infrastructure/database/dataSource.ts
+++ src/infrastructure/database/dataSource.ts
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { DataSource } from 'typeorm';
 import { ScoreEntity } from './entity/score.entity';
 import { ContentEntity } from './entity/content.entity';
@@ -7,10 +9,10 @@ export async function getDataSource() {
   if (dataSource == null) {
     const _dataSource = new DataSource({
       type: 'mysql',
-      host: '127.0.0.1',
+      host: process.env.MYSQL_HOST,
       port: 3306,
-      username: 'root',
-      password: 'root',
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
       database: 'natsukashiimono',
       // FIXME: Windows10だとパスで指定する方法が通らないというバグがある。型の列挙で暫定対応
       // entities: [__dirname + '/../**/*.entity{.ts,.js}'],
