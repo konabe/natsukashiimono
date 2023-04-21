b8c79401c06149b339141b0791b8e92cfec711b9
Ryota Konabe <rt.konabe@gmail.com>
2023-04-22 08:05:25 +0900

    対応漏れの修正

--- src/infrastructure/aws.ts
+++ src/infrastructure/aws.ts
@@ -5,5 +5,5 @@ import { CognitoIdentityServiceProvider } from 'aws-sdk';
 export const cognito = new CognitoIdentityServiceProvider({
   region: 'ap-northeast-1',
 });
-export const AWS_COGNITO_APP_CLIENT_ID = process.env.AWS_COGNITO_APP_CLIENT_ID;
-export const AWS_COGNITO_USER_POOL_ID = process.env.AWS_COGNITO_USER_POOL_ID;
+export const AWS_COGNITO_APP_CLIENT_ID = process.env.AWS_COGNITO_APP_CLIENT_ID!;
+export const AWS_COGNITO_USER_POOL_ID = process.env.AWS_COGNITO_USER_POOL_ID!;
