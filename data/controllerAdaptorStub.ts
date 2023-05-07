67f89dc07faad778ec410d6aaaeaa661d9b4f77c
Ryota Konabe <rt.konabe@gmail.com>
2023-05-07 20:09:17 +0900

    fix ControllerAdaptor stub

--- data/controllerAdaptorStub.ts
+++ data/controllerAdaptorStub.ts
@@ -1,23 +1,24 @@
 import { Response } from 'express';
 import {
   BaseRequest,
+  BaseResponse,
   ControllerAdaptor,
   ValidatedOptions,
 } from '../src/controller/controllerAdaptor';
 
 class RequestForStub extends BaseRequest {}
+class ResponseForStub extends BaseResponse {}
 
-export class ControllerAdaptorStub extends ControllerAdaptor<RequestForStub> {
+export class ControllerAdaptorStub extends ControllerAdaptor<
+  RequestForStub,
+  ResponseForStub
+> {
   allowed = ['admin', 'user'];
   createRequest(_: any): RequestForStub {
     return new RequestForStub();
   }
-  async validated(
-    _: RequestForStub,
-    res: Response<any, Record<string, any>>,
-    __: ValidatedOptions,
-  ): Promise<void> {
-    res.status(200).json();
+  async validated(_: RequestForStub, __: ValidatedOptions): Promise<void> {
+    this.returnWithSuccess(new ResponseForStub());
     await new Promise((resolve) => setTimeout(resolve, 0));
     return;
   }
